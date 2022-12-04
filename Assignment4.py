5304e32c3161fbc29b400d7d3fdcfe499c18afdc
AnujSharma100 <117298925+AnujSharma100@users.noreply.github.com>
2022-12-04 23:57:02 +0530

    Add files via upload

--- Assignment4.py
+++ Assignment4.py
@@ -1,2 +1,11 @@
-r= lambda a : a+25
-print(r(10))
+# assignment-4 
+
+# r= lambda a : a+25
+# print(r(10))
+
+# Q-2
+numbers = [20,47,61,16,81,88]
+print('original list:',numbers)
+result= map (lambda x:x+x+x, numbers)
+print("\ntriple of said list numbers:")
+print(list(result))
